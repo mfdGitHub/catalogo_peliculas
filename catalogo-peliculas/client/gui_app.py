1eed91c042d8d20a4ac9643dd54159c318dcac11
MoisesDebian11 <moyinformatico@gmail.com>
2022-12-24 20:13:53 -0600

    Se agregaron los campos de texto y los botones.

--- catalogo-peliculas/client/gui_app.py
+++ catalogo-peliculas/client/gui_app.py
@@ -40,3 +40,29 @@ class Frame(tk.Frame):
         self.label_genero = tk.Label(self, text = 'Genero: ')
         self.label_genero.config(font=('Arial',12,'bold'))
         self.label_genero.grid(row=2, column=0, padx=10, pady=10)
+
+        # Entry de cada campo
+        self.entry_nombre = tk.Entry(self)
+        self.entry_nombre.config(width=50, state='disabled', font=('Arial',12))
+        self.entry_nombre.grid(row=0, column=1, padx=10, pady=10, columnspan=2)
+
+        self.entry_duracion = tk.Entry(self)
+        self.entry_duracion.config(width=50, state='disabled', font=('Arial',12))
+        self.entry_duracion.grid(row=1, column=1, padx=10, pady=10, columnspan=2)
+
+        self.entry_genero = tk.Entry(self)
+        self.entry_genero.config(width=50, state='disabled', font=('Arial',12))
+        self.entry_genero.grid(row=2, column=1, padx=10, pady=10, columnspan=2)
+
+        # Botones
+        self.boton_nuevo = tk.Button(self, text='Nuevo')
+        self.boton_nuevo.config(width=20, font=('Arial', 12, 'bold'), fg='#DAD5D6', bg='#158645', cursor='hand2', activebackground='#35BD6F')
+        self.boton_nuevo.grid(row=4, column=0, padx=10, pady=10)
+
+        self.boton_guardar = tk.Button(self, text='Guardar')
+        self.boton_guardar.config(width=20, font=('Arial', 12, 'bold'), fg='#DAD5D6', bg='#1658A2', cursor='hand2', activebackground='#3586DF')
+        self.boton_guardar.grid(row=4, column=1, padx=10, pady=10)
+
+        self.boton_cancelar = tk.Button(self, text='Cancelar')
+        self.boton_cancelar.config(width=20, font=('Arial', 12, 'bold'), fg='#DAD5D6', bg='#BD152E', cursor='hand2', activebackground='#E15370')
+        self.boton_cancelar.grid(row=4, column=2, padx=10, pady=10)
